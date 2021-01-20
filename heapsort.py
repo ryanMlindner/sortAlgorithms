792d66d1996661c5c1e41885e044a5d7462aca43
Ryan Lindner <ryan.m.lindner@gmail.com>
2021-01-20 16:38:55 -0700

    Update heapsort.py
    added iterative option of heapify to the PDL

--- heapsort.py
+++ heapsort.py
@@ -15,10 +15,10 @@
 
 ##build a heap with the size of the array
 # for each index in the array from max length down to 1
-# heapify recursively(max binary log of size nested calls)
+# heapify recursively(max binary log of size nested calls) or iterates through a dynamic length stack
 # when heapify ends the array will satisfy the heap property
 
-##heapify changes the order of items in the array in order to maintain the heap property
+##heapifyRecursive changes the order of items in the array in order to maintain the heap property
 # at any given index (i) in the heap,
 # compare the values of i and the left and right children of i
 # determine the largest
@@ -28,6 +28,17 @@
 # to fix any violations of the heap property
 # caused by exchanging (left or right) with i
 # if the largest is the original index, the tree at (i) is a heap, no action required by heapify
+
+##heapifyIterative has the same goal, but uses a control loop instead of recursion
+# define a stack to use for flagging the end of the loop
+# pass in an amount of elements to add to the stack,
+# will always be the front half of the array (called from build heap) or one element
+# while there are items in the stack
+# pop the first item, do internal logic of heapify
+# (compare left, right, i -> largest goes to A[i])
+# if largest is NOT original index
+# swap largest and original, add the index of largest to the stack
+# ends when stack is empty
 
 ##heapsort uses the built heap to sort into an ordered array of size n
 # iterate over the array from max length down to index (2)
